1814ba6388dd65f7042c9f8a99e878fdb0f64a5e
Christofer Chan <devchumbing@gmail.com>
2018-06-23 05:30:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,10 +17,9 @@ def get_contestant_name(data, occupation)
       k.each do |key, value|
    if value == occupation
      return k["name"]
-
+        end
+      end
     end
-  end
-  end
   end
 end
 
