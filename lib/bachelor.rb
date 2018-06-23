a58a009d5feca284d05075f6c4054206d1d609e1
Christofer Chan <devchumbing@gmail.com>
2018-06-23 03:54:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,8 @@
 def get_first_name_of_season_winner(data, season)
   def winner(data, season)
   data[season].each_with_index do |name, index|
-    if dataseason][index].has_value?("Winner")
-      return dataseason][index][:"name"].split(" ")[index]
+    if data[season][index].has_value?("Winner")
+      return data[season][index][:"name"].split(" ")[index]
       end
   end
 end
